f0b58ccc33fccd61cd4f4a1092f3125f25fd9f69
Jefferson Eduardo <64866460+BlackBoltEdu@users.noreply.github.com>
2021-09-19 03:11:50 -0300

    Removendo classe desnecessária
    Remoção de classe teste

--- assets/javascript/script.js
+++
@@ -1,9 +0,0 @@
-let btnSubmit = document.querySelector('#btn-submit');
-let formLogin = document.querySelector('#form-login');
-
-formLogin.addEventListener('submit', e =>{
-    e.preventDefault;
-    let email = document.querySelector('#email').value;
-    let password = document.querySelector('#password').value;
-    console.log(`Aqui está seu e-mail: ${email} e aqui está sua senha ${password}`);
-});
